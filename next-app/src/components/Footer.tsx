df5b05e82d27fdcb7f67e86646b92df7042b256f
いつか <mi960n5@gmail.com>
2026-01-11 18:24:09 +0900

    Add files via upload

---
+++ next-app/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div id="footer">
+      <p>
+        ※このサイトはファンによる非公式のお誕生日お祝いサイトです。
+        <br />
+        碧依さくらさんご本人・公式とは一切関係ありません。
+      </p>
+    </div>
+  );
+}
